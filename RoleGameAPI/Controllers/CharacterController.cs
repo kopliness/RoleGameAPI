5ca05c976b5d5b85b2a3191184fb8af561269eb1
Kopliness <mr.vladlis3568@gmail.com>
2023-07-29 15:37:42 +0300

    added delete method

--- RoleGameAPI/Controllers/CharacterController.cs
+++ RoleGameAPI/Controllers/CharacterController.cs
@@ -28,11 +28,21 @@ namespace RoleGameAPI.Controllers
         {
             return Ok(await _characterService.AddCharacter(newCharacter)); 
         }
-        [HttpPut]
+        [HttpPut()]
         public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
         {
             var response = await _characterService.UpdateCharacter(updatedCharacter);
             if(response.Data is null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> DeleteCharacter(int id)
+        {
+            var response = await _characterService.DeleteCharacter(id);
+            if (response.Data is null)
             {
                 return NotFound(response);
             }
--- RoleGameAPI/Services/CharacterService/ICharacterService.cs
+++ RoleGameAPI/Services/CharacterService/ICharacterService.cs
@@ -6,6 +6,8 @@
         Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
         Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
         Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
+        Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id);
+
 
     }
 }
