e97f24af1623942e9d141503fdd8ad605af4aee0
Kopliness <mr.vladlis3568@gmail.com>
2023-07-29 13:20:10 +0300

    added character service and modified controller

--- RoleGameAPI/Controllers/CharacterController.cs
+++ RoleGameAPI/Controllers/CharacterController.cs
@@ -6,21 +6,27 @@ namespace RoleGameAPI.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = new List<Character>
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
         {
-            new Character(),
-            new Character(){ Id=1,Name="Trall", Class=RpgClass.Rogue}
-        };
+            _characterService = characterService;
+        }
 
         [HttpGet("GetAll")]
         public ActionResult<List<Character>> Get()
         {
-            return Ok(characters);
+            return Ok(_characterService.GetAllCharacters());
         }
         [HttpGet("{id}")]
         public ActionResult<List<Character>> GetSingle(int id)
         {
-            return Ok(characters.FirstOrDefault(c => c.Id==id));
+            return Ok(_characterService.GetCharacterById(id));
+        }
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            return Ok(_characterService.AddCharacter(newCharacter)); 
         }
     }
 }
---
+++ RoleGameAPI/Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,11 @@
+﻿using RoleGameAPI.Models;
+
+namespace RoleGameAPI.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+        Character GetCharacterById(int id);
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
