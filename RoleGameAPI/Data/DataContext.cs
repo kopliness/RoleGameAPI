ac51d2b629a96bb08c047666f781ef8bc1956abc
Kopliness <mr.vladlis3568@gmail.com>
2023-07-29 16:15:04 +0300

    created dbcontext and first migration

---
+++ RoleGameAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace RoleGameAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        { 
+
+        }
+        public DbSet<Character> Characters => Set<Character>();
+
+    }
+}
